157530374ca369788d369b484cc95939bb574893
Brady Wied <brady.wied@fusionauth.io>
2024-03-11 22:39:59 -0600

    This is what was needed

--- src/main/java/brady/action/AuthNeededAction.java
+++ src/main/java/brady/action/AuthNeededAction.java
@@ -2,9 +2,11 @@ package brady.action;
 
 import com.google.inject.Inject;
 import org.primeframework.mvc.action.annotation.Action;
+import org.primeframework.mvc.action.result.annotation.Redirect;
 import org.primeframework.mvc.security.UserLoginSecurityContext;
 
 @Action(requiresAuthentication = true, constraints = {"somerole"})
+@Redirect(code = "unauthenticated", uri = "/")
 public class AuthNeededAction {
     @Inject
     private UserLoginSecurityContext securityContext;
