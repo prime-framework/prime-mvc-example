1b0bc4c1a3ba2302966fb08c3344fc55b9b90414
Brady Wied <brady.wied@fusionauth.io>
2024-03-11 21:43:37 -0600

    Something that runs

--- src/main/java/brady/PrimeTest.java
+++ src/main/java/brady/PrimeTest.java
@@ -1,10 +1,27 @@
 package brady;
 
-import com.google.inject.Guice;
-import org.primeframework.mvc.guice.MVCModule;
+import com.google.inject.Module;
+import io.fusionauth.http.server.HTTPListenerConfiguration;
+import io.fusionauth.http.server.HTTPServerConfiguration;
+import org.primeframework.mvc.BasePrimeMain;
 
-public class PrimeTest {
+public class PrimeTest extends BasePrimeMain {
     public static void main(String[] args) {
-        var injector = Guice.createInjector(new MVCModule(), new OurModule());
+        var instance = new PrimeTest();
+        instance.registerShutdownHook();
+        instance.start();
+    }
+
+    @Override
+    public HTTPServerConfiguration[] configuration() {
+        var httpConfig = new HTTPServerConfiguration().withListener(new HTTPListenerConfiguration(8080));
+        return new HTTPServerConfiguration[]{
+                httpConfig
+        };
+    }
+
+    @Override
+    protected Module[] modules() {
+        return new Module[]{new OurModule()};
     }
 }
