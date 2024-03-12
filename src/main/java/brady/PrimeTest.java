65f606193bac34d2496f010d42c607354f18676b
Brady Wied <brady.wied@fusionauth.io>
2024-03-11 21:19:35 -0600

    got some basics

--- src/main/java/brady/PrimeTest.java
+++ src/main/java/brady/PrimeTest.java
@@ -1,7 +1,10 @@
 package brady;
 
+import com.google.inject.Guice;
+import org.primeframework.mvc.guice.MVCModule;
+
 public class PrimeTest {
     public static void main(String[] args) {
-        var injector = Guice.createInjector();
+        var injector = Guice.createInjector(new MVCModule());
     }
 }
